4d8bf70ee2d21e36e67645fdb1a7f987161c8781
FacundoDavidFlores <FacundoDFlores2@gmail.com>
2022-12-04 18:46:44 -0300

    Este es mi primer commit

---
+++ AppCoder/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def saludo(request):    #Consulta o peticion
+    return HttpResponse("Hola mi primera app")
+def saludo_dos(request):
+    return HttpResponse("Este es otro saludo")
+def saludo_a(request, nombre):
+    return HttpResponse(f"Hola como estas {nombre}")
+def mostrar_mi_template(request):
+    return render(request, "AppCoder/index.html")
